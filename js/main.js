6b1a113138610c06066509c169ad77bb9fea63a1
giorgiameffe <giorgiameffe03@gmail.com>
2025-02-25 18:50:00 +0100

    feat: add if

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@
 for (let i = 1; i <= 100; i++) {
 
     console.log(i);
-}
+
+    // istruzioni condizionali
+
+    if (i % 3 === 0) {
+        
+        let word = 'Fizz';
+        console.log(word);
+    
+    }
+} 
